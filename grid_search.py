c5527bdcc911a52dcab5b24b5abdc746c9e2d3ba
Fangwei <ucapfdo@ucl.ac.uk>
2021-08-20 03:56:54 +0800

    Add grid search for DT,RF and XGB

--- grid_search.py
+++ grid_search.py
@@ -149,3 +149,125 @@ def grid_search_knn(sampling, X_test, y_test, X_train, y_train):
     print('The f1 score for the testing data:', f1_test)
     auprc = average_precision_score(y_test, y_pred_proba)
     return f1_train, f1_test, auprc, conf_matrix, gridsearch
+
+def grid_search_dt(sampling, X_test, y_test, X_train, y_train):
+    """This function uses grid search to find the hyperparameters with the best f1 score for Decision Tree.
+    Output: training f1, test f1, auprc, confusion matrix and the best classifier"""
+    dt = DecisionTreeClassifier()
+    pipeline = Pipeline(steps=[['sampling', sampling],
+                               ['classifier', dt]])
+    param_grid_ = {
+        'criterion': ['gini', 'entropy'],
+        'splitter': ['random', 'best'],
+        'max_depth': [1, 2, 10],
+        'min_samples_leaf': [1, 2, 10]
+    }
+    param_grid_clf = {
+        'classifier__criterion': ['gini', 'entropy'],
+        'classifier__splitter': ['random', 'best'],
+        'classifier__max_depth': [1, 2, 10],
+        'classifier__min_samples_leaf': [1, 2, 10]
+    }
+    if sampling is None:
+        estimator = dt
+        param_grid = param_grid_
+    else:
+        estimator = pipeline
+        param_grid = param_grid_clf
+    # Fitting grid search to the train data with 5 folds
+    gridsearch = GridSearchCV(estimator=estimator,
+                              param_grid=param_grid,
+                              cv=StratifiedKFold(
+                                  n_splits=5, random_state=1, shuffle=True),
+                              scoring='f1')
+    gridsearch.fit(X_train, y_train)
+    y_pred = gridsearch.predict(X_test)
+    y_pred_proba = gridsearch.predict_proba(X_test)[:, 1]
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    print("Best: %f using %s" %
+          (gridsearch.best_score_, gridsearch.best_params_))
+    # Calculating and printing the f1 score
+    f1_train = gridsearch.best_score_
+    f1_test = f1_score(y_test, y_pred)
+    print('The f1 score for the testing data:', f1_test)
+    auprc = average_precision_score(y_test, y_pred_proba)
+    return f1_train, f1_test, auprc, conf_matrix, gridsearch
+
+def grid_search_rf(sampling, X_test, y_test, X_train, y_train):
+    """This function uses grid search to find the hyperparameters with the best f1 score for Random Forest.
+    Output: training f1, test f1, auprc, confusion matrix and the best classifier"""
+    rf = RandomForestClassifier()
+    pipeline = Pipeline(steps=[['sampling', sampling],
+                               ['classifier', rf]])
+    param_grid_ = {
+        'max_depth': [2, 5, 10, 20],
+        'n_estimators': [10, 100, 1000],
+    }
+    param_grid_clf = {
+        'classifier__max_depth': [2, 5, 10, 20],
+        'classifier__n_estimators': [10, 100, 1000],
+        # 'classifier__criterion': ['gini', 'entropy']
+    }
+    if sampling is None:
+        estimator = rf
+        param_grid = param_grid_
+    else:
+        estimator = pipeline
+        param_grid = param_grid_clf
+    # Fitting grid search to the train data with 5 folds
+    gridsearch = GridSearchCV(estimator=estimator,
+                              param_grid=param_grid,
+                              cv=StratifiedKFold(
+                                  n_splits=5, random_state=1, shuffle=True),
+                              scoring='f1')
+    gridsearch.fit(X_train, y_train)
+    y_pred = gridsearch.predict(X_test)
+    y_pred_proba = gridsearch.predict_proba(X_test)[:, 1]
+    print("Best: %f using %s" %
+          (gridsearch.best_score_, gridsearch.best_params_))
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    # Calculating and printing the f1 score
+    f1_train = gridsearch.best_score_
+    f1_test = f1_score(y_test, y_pred)
+    print('The f1 score for the testing data:', f1_test)
+    auprc = average_precision_score(y_test, y_pred_proba)
+    return f1_train, f1_test, auprc, conf_matrix, gridsearch
+
+def grid_search_xgb(sampling, X_test, y_test, X_train, y_train):
+    """This function uses grid search to find the hyperparameters with the best f1 score for XGBoost.
+    Output: training f1, test f1, auprc, confusion matrix and the best classifier"""
+    xgboost = XGBClassifier()
+    pipeline = Pipeline(steps=[['sampling', sampling],
+                               ['classifier', xgboost]])
+    param_grid_ = {
+        'n_estimators': [100, 1000, 10000],
+        'learning_rate': [0.01, 0.1, 0.2]
+    }
+    param_grid_clf = {
+        'classifier__n_estimators': [100, 1000, 10000],
+        'classifier__learning_rate': [0.01, 0.1, 0.2]
+    }
+    if sampling is None:
+        estimator = xgboost
+        param_grid = param_grid_
+    else:
+        estimator = pipeline
+        param_grid = param_grid_clf
+    # Fitting grid search to the train data with 5 folds
+    gridsearch = GridSearchCV(estimator=estimator,
+                              param_grid=param_grid,
+                              cv=StratifiedKFold(
+                                  n_splits=5, random_state=1, shuffle=True),
+                              scoring='f1')
+    gridsearch.fit(X_train, y_train)
+    y_pred = gridsearch.predict(X_test)
+    y_pred_proba = gridsearch.predict_proba(X_test)[:, 1]
+    print("Best: %f using %s" %
+          (gridsearch.best_score_, gridsearch.best_params_))
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    # Calculating and printing the f1 score
+    f1_train = gridsearch.best_score_
+    f1_test = f1_score(y_test, y_pred)
+    print('The f1 score for the testing data:', f1_test)
+    auprc = average_precision_score(y_test, y_pred_proba)
+    return f1_train, f1_test, auprc, conf_matrix, gridsearch
